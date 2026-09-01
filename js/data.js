bef33b30db7a36c32fe335b4056a1ce14196dc5d
ahmedgaberelshemy-gif <ahmed.gaber.elshemy@gmail.com>
2026-09-01 20:23:07 +0300

    Update Programming track with exact 11 courses structure

--- js/data.js
+++ js/data.js
@@ -703,28 +703,64 @@ const programmingCoursesData = [
     "color": "indigo"
   },
   {
-    "id": "html",
-    "title": "كورس HTML",
+    "id": "html_intro",
+    "title": "كورس HTML - شرح أول مرة",
     "icon": "fa-code",
     "color": "orange"
   },
   {
-    "id": "css",
-    "title": "كورس CSS",
+    "id": "html_review",
+    "title": "كورس HTML - مراجعة وتطوير",
+    "icon": "fa-wrench",
+    "color": "amber"
+  },
+  {
+    "id": "css_intro",
+    "title": "كورس CSS - شرح أول مرة",
     "icon": "fa-palette",
     "color": "blue"
   },
   {
-    "id": "js",
-    "title": "كورس JavaScript",
+    "id": "css_review",
+    "title": "كورس CSS - مراجعة وتطوير",
+    "icon": "fa-wand-magic-sparkles",
+    "color": "sky"
+  },
+  {
+    "id": "js_intro",
+    "title": "كورس JavaScript - شرح أول مرة",
     "icon": "fa-bolt",
+    "color": "yellow"
+  },
+  {
+    "id": "js_review",
+    "title": "كورس JavaScript - مراجعة وتطوير",
+    "icon": "fa-gears",
     "color": "amber"
+  },
+  {
+    "id": "python_intro",
+    "title": "كورس بايثون (Python) - شرح أول مرة",
+    "icon": "fa-terminal",
+    "color": "emerald"
+  },
+  {
+    "id": "python_review",
+    "title": "كورس بايثون (Python) - مراجعة وتطوير",
+    "icon": "fa-microchip",
+    "color": "teal"
   },
   {
     "id": "bootstrap",
     "title": "كورس بوت ستراب (Bootstrap)",
     "icon": "fa-cubes",
     "color": "purple"
+  },
+  {
+    "id": "ai",
+    "title": "كورس ذكاء اصطناعي",
+    "icon": "fa-brain",
+    "color": "rose"
   }
 ];
 
